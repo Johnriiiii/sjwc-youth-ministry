7a3ccc56eb45ecea3c2c82738e4b01d3b455d037
Johnriiiii <125574722+Johnriiiii@users.noreply.github.com>
2026-03-26 17:26:53 +0800

    Hide subtitle tag and convert chapel name to uppercase

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -9,8 +9,8 @@ export function Header({ subtitle = 'Youth Ministry' }: HeaderProps) {
         <img src="/SWJC-YOUTH.png" alt="St. Joseph the Worker Chapel Youth Council" className="logo" />
       </div>
       <div className="header-text">
-        <div className="chapel-name">St. Joseph the Worker Chapel</div>
-        <div className="chapel-tag">✦ {subtitle} ✦</div>
+        <div className="chapel-name">ST. JOSEPH THE WORKER CHAPEL</div>
+        {subtitle && <div className="chapel-tag">✦ {subtitle} ✦</div>}
       </div>
     </header>
   )
